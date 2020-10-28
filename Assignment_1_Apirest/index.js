b7bd85f38f35c6f5b68e6bf8ec7fc57bcd0300e5
Roger Colquehuanca Calcina <rogcolquehuancac@upt.pe>
2020-10-27 19:39:53 -0500

    adding script nodemon

--- Assignment_1_Apirest/index.js
+++ Assignment_1_Apirest/index.js
@@ -19,15 +19,15 @@ app.use('/leaders', leaderRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-console.log(req.headers);
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
